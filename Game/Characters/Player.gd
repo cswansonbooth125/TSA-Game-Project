2d9a2d34f9972ec800aa70aeea503e811be665f1
cswansonbooth125 <143012596+cswansonbooth125@users.noreply.github.com>
2023-09-09 20:24:43 -0600

    Added player sprite and made a script to make it move

---
+++ Game/Characters/Player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var moveSpeed : float = 100
+
+func _physics_process(_delta):
+	#get input direction
+	var inputDirection = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	print(inputDirection)
+	#update velocity
+	velocity = inputDirection * moveSpeed
+	#move and slide function
+	move_and_slide()
