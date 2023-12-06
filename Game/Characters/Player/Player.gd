ede497af65664eb8ce9fbf9bdb8ab7d1c38d7300
cswansonbooth125 <cswansonbooth125@gmail.com>
2023-12-05 22:36:22 -0700

    added death and health to player

--- Game/Characters/Player/Player.gd
+++ Game/Characters/Player/Player.gd
@@ -2,15 +2,48 @@ extends CharacterBody2D
 
 class_name Player
 
-const max_speed = 200
+const max_speed = 500
+var health = 3
+var dead = false
+var enemy_attack_cooldown = true
+var enemy_in_hitbox = false
+
 var input = Vector2.ZERO
 var current_dir = "down"
 
-
+var spell_cooldown = true
+var spell = preload("res://Scenes/spell_attack.tscn")
 
 func _physics_process(delta):
-	player_movement(delta)
+	player_movement()
+	enemy_attack()
 	
+	if health <= 0:
+		dead = true
+		
+	
+func player_movement():
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		velocity = Vector2.ZERO
+	else:
+		velocity = max_speed * input
+	
+	move_and_slide()
+	
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("left_mouse") and spell_cooldown:
+		spell_cooldown = false
+		var spell_instance = spell.instantiate()
+		spell_instance.rotation = $Marker2D.rotation
+		spell_instance.global_position = $Marker2D.global_position
+		add_child(spell_instance)
+		
+		await get_tree().create_timer(0.2).timeout
+		spell_cooldown = true
 
 func get_input():
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
@@ -32,16 +65,6 @@ func get_input():
 		play_anim(0)
 	
 	return input.normalized()
-
-func player_movement(_delta):
-	input = get_input()
-	
-	if input == Vector2.ZERO:
-		velocity = Vector2.ZERO
-	else:
-		velocity = max_speed * input
-	
-	move_and_slide()
 
 func play_anim(movement):
 	var dir = current_dir
@@ -69,5 +92,21 @@ func play_anim(movement):
 			elif movement == 0:
 				anim.play("idle_up")
 
-func player():
-	pass
+func _on_player_hitbox_body_entered(body):
+	if body.is_in_group("enemy"):
+		enemy_in_hitbox = true
+
+func _on_player_hitbox_body_exited(body):
+	if body.is_in_group("enemy"):
+		enemy_in_hitbox = false
+
+func enemy_attack():
+	if enemy_in_hitbox and enemy_attack_cooldown:
+		health -= 1
+		enemy_attack_cooldown = false
+		$Damage_cooldown.start()
+		print(health)
+
+
+func _on_damage_cooldown_timeout():
+	enemy_attack_cooldown = true
