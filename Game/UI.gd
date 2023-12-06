d087981554b952775d99e24ebf3885d671e577cc
cswansonbooth125 <cswansonbooth125@gmail.com>
2023-12-05 22:38:56 -0700

    created ui for player

--- Game/UI.gd
+++ Game/UI.gd
@@ -1,18 +1,4 @@
 extends CanvasLayer
 class_name UI
 
-@onready var health_label = $Health
 
-var health = 3:
-	set(new_health):
-		health = new_health
-# Called when the node enters the scene tree for the first time.
-
-
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
